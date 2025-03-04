1efaf735d8981ea8b9046373ed43b7512f3afa57
Abdul Irfan <unknown7k7k@gmail.com>
2025-03-04 17:26:57 +0530

    chore: add eslint-plugin-filenames-simple

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,23 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
+import filenameSimple from 'eslint-plugin-filenames-simple';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   prettierConfig,
+  {
+    plugins: {
+      'filename-simple': filenameSimple,
+    },
+    rules: {
+      'filename-simple/naming-convention': [
+        'error',
+        {
+          rule: 'camelCase',
+        },
+      ],
+    },
+  },
 );
