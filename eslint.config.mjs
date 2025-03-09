f088f0d43d7a984b51341e9ed36e1f6c6162ec41
Abdul Irfan <unknown7k7k@gmail.com>
2025-03-09 13:37:25 +0530

    chore: add eslint-plugin-import and configure import roles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,20 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
 import filenameSimple from 'eslint-plugin-filenames-simple';
+import importPlugin from 'eslint-plugin-import';
+import tsParser from '@typescript-eslint/parser';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   prettierConfig,
+  importPlugin.flatConfigs['recommended'],
   {
     plugins: {
       'filename-simple': filenameSimple,
+    },
+    languageOptions: {
+      parser: tsParser,
     },
     rules: {
       'filename-simple/naming-convention': [
@@ -20,6 +26,17 @@ export default tseslint.config(
           rule: 'camelCase',
         },
       ],
+      'import/order': 'error',
+      'import/named': 'error',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
     },
   },
 );
