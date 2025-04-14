a666c312f98f62fe40c275c8fcfe389722c27df0
Abdul Irfan <unknown7k7k@gmail.com>
2025-04-14 09:37:43 +0530

    feat: add Zod validation middleware

---
+++ apps/user-service/src/infrastructure/http/middleware/schemaValidator.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, z } from 'zod';
+import logger from '@utils/logger';
+
+export const schemaValidator = (
+  validationSchema: z.AnyZodObject | z.ZodOptional<AnyZodObject>,
+) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const validatedData = await validationSchema.parseAsync(req.body);
+      req.body = validatedData;
+      next();
+    } catch (error) {
+      let validationError = error;
+
+      if (error instanceof z.ZodError) {
+        validationError = error.issues.map(issue => ({
+          path: issue.path[0],
+          message: issue.message,
+        }));
+
+        logger.debug('Schema validation failed', {
+          path: req.path,
+          errors: validationError,
+        });
+      }
+
+      return res.status(400).json({
+        status: 'failed',
+        error: validationError,
+      });
+    }
+  };
+};
