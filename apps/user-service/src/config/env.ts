b72c3660bd48f77d0f523ef5d45200d6a9594c2d
Abdul Irfan <unknown7k7k@gmail.com>
2025-04-15 07:18:48 +0530

    feat: update Zod env schema parse

--- apps/user-service/src/config/env.ts
+++ apps/user-service/src/config/env.ts
@@ -6,7 +6,19 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.coerce.string(),
+  JWT_ACCESS_TOKEN_SECRET: z.coerce.string(),
+  JWT_REFRESH_TOKEN_SECRET: z.coerce.string(),
+  JWT_ACCESS_TOKEN_EXPIRES_IN: z.coerce.string().default('1h'),
+  JWT_REFRESH_TOKEN_EXPIRES_IN: z.coerce.string().default('30d'),
 });
 
 const env = envSchema.parse(process.env);
-export const { NODE_ENV, PORT, DATABASE_URL } = env;
+export const {
+  NODE_ENV,
+  PORT,
+  DATABASE_URL,
+  JWT_ACCESS_TOKEN_EXPIRES_IN,
+  JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_EXPIRES_IN,
+  JWT_REFRESH_TOKEN_SECRET,
+} = env;
